9daddb45a408ef8b486a0f2dc4e52998a10f8261
ErinsonBorrayo <borrayo.erinson@gmail.com>
2016-04-03 14:39:26 -0600

    CRUD Autor
    Funciones CRUD para Autor terminadas con éxito

--- ManejadorAutor.java
+++ ManejadorAutor.java
@@ -21,7 +21,35 @@ public class ManejadorAutor {
 	public void agregarAutor(Autor autor) {
 		this.listaAutores.add(autor);
 	}
+
+	public void modificarAutor(Autor autorAnt,String nombreAutor) {
+		Autor autor = new Autor();
+		boolean existenciaAutor = false;
+		for (Autor aaTemp: listaAutores) {
+			if (aaTemp.getNombreAutor().equals(nombreAutor)) {
+				System.out.println("Este autor ya existe");
+				System.out.println("Imposible de modificar");
+				System.out.println("");
+				existenciaAutor = true;
+				break;
+			} else {
+				existenciaAutor = false;
+			}
+		}
+		if (existenciaAutor == false) {
+			autor.setId(autorAnt.getId());
+			autor.setNombreAutor(nombreAutor);
+			System.out.println("Autor: " + autorAnt.getId() + " modificado exitosamente");
+			listaAutores.remove(autorAnt);
+			listaAutores.add(autor);
+		}
+	}
 	
+	public void eliminarAutor(Autor autor) {
+		listaAutores.remove(autor);
+		System.out.println("Autor " + autor.getNombreAutor() + " eliminado exitosamente!");
+	}
+
 	public ArrayList<Autor> getListaAutores() {
 		return listaAutores;
 	}
