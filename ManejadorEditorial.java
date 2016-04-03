2476ca0fa26b549f54dc5fbba67992a17330224f
ErinsonBorrayo <borrayo.erinson@gmail.com>
2016-04-03 16:11:32 -0600

    CRUD EDITORIAL
    Funciones CRUD Editorial terminada

--- ManejadorEditorial.java
+++ ManejadorEditorial.java
@@ -22,8 +22,35 @@ public class ManejadorEditorial {
 		this.listaEditoriales.add(editorial);
 	}
 	
+	public void modificarEditorial(Editorial editorialAnt,String nombreEditorial) {
+		Editorial editorial = new Editorial();
+		boolean existenciaEditorial = false;
+		for (Editorial edTemp: listaEditoriales) {
+			if (edTemp.getNombreEditorial().equals(nombreEditorial)) {
+				System.out.println("Esta editorial ya existe");
+				System.out.println("Imposible de modificar");
+				System.out.println("");
+				existenciaEditorial = true;
+				break;
+			} else {
+				existenciaEditorial = false;
+			}
+		}
+		if (existenciaEditorial == false) {
+			editorial.setId(editorialAnt.getId());
+			editorial.setNombreEditorial(nombreEditorial);
+			System.out.println("Editorial: " + editorialAnt.getId() + " modificado exitosamente");
+			listaEditoriales.remove(editorialAnt);
+			listaEditoriales.add(editorial);
+		}
+	}
+	
+	public void eliminarEditorial(Editorial editorial) {
+		listaEditoriales.remove(editorial);
+		System.out.println("Editorial " + editorial.getNombreEditorial() + " eliminado exitosamente!");
+	}
+	
 	public ArrayList<Editorial> getListaEditoriales() {
 		return listaEditoriales;
 	}
-	
 }
